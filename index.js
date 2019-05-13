7217c4bf849bdd7c70e940aa4ba9d9e8594f1eed
root <root@DESKTOP-RENKH2D.localdomain>
2019-05-13 15:37:47 +0800

    add js file

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require('http')
+
+const server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end("hello node");
+});
+
+server.listen(3000);
