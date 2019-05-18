a0632cad7273b904cc64e1bcd54ffa9e1fc44b95
jinelei <jinelei@163.com>
2019-05-18 23:49:33 +0800

    更新

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http')
 
 const server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end("hello node");
+    res.end("hello nodejs");
 });
 
 server.listen(3000);
