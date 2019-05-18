3cbd0f4f8951116edb6db4ca33159f09af696004
jinelei <jinelei@163.com>
2019-05-18 23:48:48 +0800

    更新看门狗

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # nodejs
+
+- *watchdog.sh*  
+使用`inotifywait`监听当前路径下的所有文件，监听到变化时重启`index.js`。
--- watchdog.sh
+++ watchdog.sh
@@ -1,20 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
-watchFile() {
-    inotifywait -rmq -e modify -e create -e delete_self -e move `pwd` | while read file
-    do
-        restart
-    done
+watchFile(){
+    inotifywait -rmq -e modify -e create -e delete_self -e move\
+    `pwd` | while read file
+        do
+            restart
+        done
 }
-
-restart() {
+restart(){
     PID=`ps -ef |grep 'node index.js' |grep -v grep |awk '{print $2}'`
     if [ "$PID" != "" ]; then
-        echo "\033[41;32mrestart server\033[0m"
+        echo -e "\033[41;32mrestart server\033[0m"
         kill -9 $PID
         node index.js 2>&1 &
     else
-        echo "\033[41;32mstart   server\033[0m"
+        echo -e "\033[41;32mstart   server\033[0m"
         node index.js 2>&1 &
     fi
 }
